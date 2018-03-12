a3975d8ed49212a2db15a9999071684d1fd79704
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-12 18:20:21 +0000

    Add Author and Book classes with constructors, getters and setters

---
+++ src/main/java/models/Author.java
@@ -0,0 +1,51 @@
+package models;
+
+import java.util.Set;
+
+public class Author {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
