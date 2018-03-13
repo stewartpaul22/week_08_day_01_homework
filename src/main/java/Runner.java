8783b8a127cba3efaa2423e229f04a6e44d29759
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-13 08:50:37 +0000

    Add DBHelper with generic methods and removed DBAuthor and DBBook

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,4 @@
-import db.DBAuthor;
-import db.DBBook;
+import db.DBHelper;
 import models.Author;
 import models.Book;
 
@@ -9,35 +8,43 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        DBAuthor.deleteAll();
-        DBBook.deleteAll();
+        DBHelper.deleteAll("Author");
+        DBHelper.deleteAll("Book");
 
         Author author1 = new Author("John", "Niven");
-        DBAuthor.saveAuthor(author1);
+        DBHelper.save(author1);
 
         Author author2 = new Author("Douglas", "Adams");
-        DBAuthor.saveAuthor(author2);
+        DBHelper.save(author2);
 
         Book book1 = new Book("Kill Your Friends", author1);
-        DBBook.saveBook(book1);
+        DBHelper.save(book1);
 
         Book book2 = new Book("The Hitchhiker's Guide to the Galaxy", author2);
-        DBBook.saveBook(book2);
+        DBHelper.save(book2);
 
         Book book3 = new Book("The Second Coming", author1);
-        DBBook.saveBook(book3);
+        DBHelper.save(book3);
 
         book2.setTitle("The Hitchhiker's Guide to Eating Galaxies");
-        DBBook.updateBook(book2);
+        DBHelper.update(book2);
 
         author1.setFirstName("J");
-        DBAuthor.updateAuthor(author1);
+        DBHelper.update(author1);
 
-        List<Book> books = DBBook.getBooks();
+        DBHelper.delete(book3);
 
-        List<Author> authors = DBAuthor.getAuthors();
+        List<Book> books = DBHelper.getAll("Book");
 
-        List<Book> booksByAuthors = DBAuthor.getBooks(author1.getId());
+        List<Author> authors = DBHelper.getAll("Author");
+
+        List<Book> booksByAuthor1 = DBHelper.getBooks(author1.getId());
+
+        List<Book> booksByAuthor2 = DBHelper.getBooks(author2.getId());
+
+        Book foundBook = DBHelper.find(book1.getId(), "Book");
+
+        Author foundAuthor = DBHelper.find(author1.getId(), "Author");
 
     }
 
--- src/main/java/db/DBHelper.java
+++ src/main/java/db/DBHelper.java
@@ -9,17 +9,17 @@ import org.hibernate.Transaction;
 
 import java.util.List;
 
-public class DBAuthor {
+public class DBHelper {
 
     private static Transaction transaction;
     private static Session session;
 
-    public static void saveAuthor(Author author) {
+    public static void save(Object object) {
 
         session = HibernateUtil.getSessionFactory().openSession();
         try {
             transaction = session.beginTransaction();
-            session.save(author);
+            session.save(object);
             transaction.commit();
         } catch (HibernateException e) {
             transaction.rollback();
@@ -30,13 +30,28 @@ public class DBAuthor {
 
     }
 
-    public static List<Author> getAuthors() {
+    public static <T> List<T> getAll(String className){
         session = HibernateUtil.getSessionFactory().openSession();
-        List<Author> authors = null;
+        List<T> results = null;
         try {
             transaction = session.beginTransaction();
-            String hql = "from Author";
-            authors = session.createQuery(hql).list();
+            String hql = "from " + className;
+            Query query = session.createQuery(hql);
+            results = query.list();
+        } catch (HibernateException e){
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return results;
+    }
+
+    public static void update(Object object){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.update(object);
             transaction.commit();
         } catch (HibernateException e) {
             transaction.rollback();
@@ -44,15 +59,13 @@ public class DBAuthor {
         } finally {
             session.close();
         }
-        return authors;
     }
 
-    public static void updateAuthor(Author author) {
-
+    public static void delete(Object object){
         session = HibernateUtil.getSessionFactory().openSession();
         try {
             transaction = session.beginTransaction();
-            session.update(author);
+            session.delete(object);
             transaction.commit();
         } catch (HibernateException e) {
             transaction.rollback();
@@ -60,14 +73,13 @@ public class DBAuthor {
         } finally {
             session.close();
         }
-
     }
 
-    public static void deleteAll(){
+    public static void deleteAll(String className){
         session = HibernateUtil.getSessionFactory().openSession();
         try {
             transaction = session.beginTransaction();
-            String hql = "delete from Author";
+            String hql = "delete from " + className;
             Query query = session.createQuery(hql);
             query.executeUpdate();
         } catch (HibernateException e){
@@ -95,6 +107,25 @@ public class DBAuthor {
             session.close();
         }
         return books;
+    }
+
+    public static <T> T find(int id, String className){
+        session = HibernateUtil.getSessionFactory().openSession();
+        T result = null;
+        try {
+            transaction = session.beginTransaction();
+            String hql = "from " +  className + " where id = :id";
+            Query query = session.createQuery(hql);
+            query.setInteger("id", id);
+            result = (T)query.uniqueResult();
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return result;
     }
 
 }
